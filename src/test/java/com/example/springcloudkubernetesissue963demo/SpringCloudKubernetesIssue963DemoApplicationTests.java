ca39424409fa08a5d9f077da27e216f2b2f201c2
Theng <wai.loon.theng@accenture.com>
2022-01-29 09:20:40 +0800

    Initial commit

---
+++ src/test/java/com/example/springcloudkubernetesissue963demo/SpringCloudKubernetesIssue963DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudkubernetesissue963demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudKubernetesIssue963DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
